b9728a86dab8c657116991ed2260bc00aeee81c8
Parking Master <parkingmaster@email.com>
2021-09-26 18:53:03 -0400

    Create no-time.js

---
+++ no-time.js
@@ -0,0 +1,13 @@
+for (var ssrc, script, s2 = document.querySelectorAll("script"), i = 0; i < s2.length; i++) scripts = s2[i], ssrc = scripts.src;
+var ms = Date.now();
+window.addEventListener("load", function() {
+    Date.now();
+    null == typeof window && "complete" === document.readyState ? location.reload() : function() {
+        window,
+        window;
+        var t = window,
+            t = (window = t, 0)
+    }(), "complete" === document.readyState && setTimeout(function() {
+        for (var t = document.querySelectorAll("script"), e = 0; e < t.length; e++) scripts = t[e], scripts.hasAttribute("src") && (scripts.src = ssrc || scripts.remove())
+    }, 1e3)
+});
