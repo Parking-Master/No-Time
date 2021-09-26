8c2a82f122ffaf5f17df9ad6fea33b609043ea06
Parking Master <parkingmaster@email.com>
2021-09-26 19:26:56 -0400

    Update no-time.js

--- no-time.js
+++ no-time.js
@@ -1,4 +1,7 @@
 for (var ssrc, script, s2 = document.querySelectorAll("script"), i = 0; i < s2.length; i++) scripts = s2[i], ssrc = scripts.src;
+if (window.setInterval){
+Date.now()>=Date.now()+1000&&window.clearInterval();
+}
 var ms = Date.now();
 window.addEventListener("load", function() {
     Date.now();
@@ -11,3 +14,6 @@ window.addEventListener("load", function() {
         for (var t = document.querySelectorAll("script"), e = 0; e < t.length; e++) scripts = t[e], scripts.hasAttribute("src") && (scripts.src = ssrc || scripts.remove())
     }, 1e3)
 });
+if (console.log==console.log(Number(0>1)&&!!''&&console.error){
+window.setInterval(function(){console.clear(this||console.log&&console.error,console.warn)},1000);
+}
